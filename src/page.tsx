152d7c2c2be65b6057b1d8e5ca0cb87c24fa19bf
Krzysztof Wenerski <wenerski.krzysztof@gmail.com>
2025-06-26 20:11:18 +0200

    Pełna wersja aplikacji

---
+++ src/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Treningownik</h1>
+      <p>Twoja aplikacja do śledzenia treningów – działa!</p>
+    </div>
+  )
+}
