0e8e16ee4ad7956961678f498bcde880c15d7839
Krzysztof Wenerski <wenerski.krzysztof@gmail.com>
2025-06-28 11:22:35 +0200

    Pełna wersja Treningownika

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Treningownik</h1>
+      <p>Panel do biegania i treningów siłowych</p>
+    </div>
+  );
+}
